79a7a962211242c33012e9ff9a24dd884af1aab5
Charles Leifer <coleifer@gmail.com>
2018-04-07 21:24:45 -0500

    more robust embedded implementation.

--- embed.py
+++ embed.py
@@ -1,4 +1,5 @@
 import atexit
+import logging
 import random
 import socket
 import subprocess
@@ -9,60 +10,128 @@ import time
 from kt import KyotoTycoon
 
 
-def run_server(server='ktserver', server_args=None):
-    """
-    Run ktserver on a random high port and return a client connected to it.
-    """
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    attempts = 0
-    while attempts < 32:
-        attempts += 1
-        port = random.randint(16000, 32000)
-        try:
-            sock.bind(('127.0.0.1', port))
-            sock.listen(1)
-            sock.close()
-            time.sleep(0.2)
-            break
-        except OSError:
-            pass
+logger = logging.getLogger(__name__)
 
-    def _run_server():
-        extra_params = server_args or []
+
+class EmbeddedServer(object):
+    def __init__(self, server='ktserver', host='127.0.0.1', port=None,
+                 database='*', server_args=None):
+        self._server = server
+        self._host = host
+        self._port = port
+        self._database = database
+        self._server_args = server_args or []
+
+        # Signals for server startup and shutdown.
+        self._server_started = threading.Event()
+        self._server_terminated = threading.Event()
+        self._server_terminated.set()  # Start off in terminated state.
+
+        # Placeholders for server process and client.
+        self._server_p = None
+        self._client = None
+
+    @property
+    def client(self):
+        if self._server_terminated.is_set():
+            raise Exception('server not running')
+        elif self._client is None:
+            self._client = KyotoTycoon(self._host, self._port)
+        return self._client
+
+    @property
+    def pid(self):
+        if not self._server_terminated.is_set():
+            return self._server_p.pid
+
+    def _run_server(self, port):
         command = [
-            server,
+            self._server,
             '-le',  # Log errors.
             '-host',
-            '127.0.0.1',
+            self._host,
             '-port',
-            str(port)] + extra_params
-        while True:
-            p = subprocess.Popen(
+            str(port)] + self._server_args + [self._database]
+
+        while not self._server_terminated.is_set():
+            self._server_p = subprocess.Popen(
                 command,
                 stderr=sys.__stderr__.fileno(),
                 stdout=sys.__stdout__.fileno())
-            finished = [False]
 
-            def cleanup():
-                finished[0] = True
-                p.terminate()
-                p.wait()
+            self._server_started.set()
+            self._server_p.wait()
+            self._client = None
 
-            atexit.register(cleanup)
-            p.wait()
-            time.sleep(10)
-            if not finished[0]:
-                raise Exception('ktserver process died')
+            time.sleep(0.1)
+            if not self._server_terminated.is_set():
+                logger.error('server process died, restarting...')
 
-    t = threading.Thread(target=_run_server)
-    t.daemon = True
-    t.start()
+        logger.info('server shutdown')
 
-    attempts = 0
-    while attempts < 20:
-        attempts += 1
-        try:
-            client = KyotoTycoon(host='127.0.0.1', port=port)
-            return client
-        except:
-            time.sleep(0.2)
+    def _stop_server(self):
+        self._server_terminated.set()
+        self._server_p.terminate()
+        self._server_p.wait()
+        self._server_p = self._client = None
+
+    def __del__(self):
+        if self._server_p:
+            self._server_p.terminate()
+
+    def run(self):
+        """
+        Run ktserver on a random high port and return a client connected to it.
+        """
+        if not self._server_terminated.is_set():
+            logger.warning('server already running')
+            return False
+
+        if self._port is None:
+            self._port = self._find_open_port()
+
+        self._server_started.clear()
+        self._server_terminated.clear()
+
+        t = threading.Thread(target=self._run_server, args=(self._port,))
+        t.daemon = True
+        t.start()
+
+        self._server_started.wait()  # Wait for server to start up.
+
+        attempts = 0
+        while attempts < 20:
+            attempts += 1
+            try:
+                self._client = KyotoTycoon(host=self._host, port=self._port)
+                return True
+            except OSError:
+                time.sleep(0.1)
+
+        self._stop_server()
+        raise Exception('Unable to connect to server on %s:%s' %
+                        (self._host, self._port))
+
+    def stop(self):
+        if self._server_terminated.is_set():
+            logger.warning('server already stopped')
+            return False
+
+        self._stop_server()
+
+    def _find_open_port(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        attempts = 0
+        while attempts < 32:
+            attempts += 1
+            port = random.randint(16000, 32000)
+            try:
+                sock.bind(('127.0.0.1', port))
+                sock.listen(1)
+                sock.close()
+                time.sleep(0.1)
+                return port
+            except OSError:
+                pass
+
+        raise Exception('Could not find open port')
