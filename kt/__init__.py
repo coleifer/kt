0b202f7bd08623f59d15e203783b7ff8b959a9a9
Charles Leifer <coleifer@gmail.com>
2018-04-09 16:26:08 -0500

    Imports.

--- kt/__init__.py
+++ kt/__init__.py
@@ -5,7 +5,9 @@ from .client import KT_JSON
 from .client import KT_MSGPACK
 from .client import KT_PICKLE
 from .client import KyotoTycoon
+from .client import TokyoTyrant
 from .embedded import EmbeddedServer
+from .embedded import EmbeddedTokyoTyrantServer
 from .exceptions import ImproperlyConfigured
 from .exceptions import KyotoTycoonError
 from .exceptions import ProtocolError
